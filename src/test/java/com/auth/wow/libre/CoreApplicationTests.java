2b73cd24d15e724e12d21096680bea9342498b35
Sebastian Chitiva <mschitiva68@ucatolica.edu.co>
2024-09-15 16:10:55 -0500

    Version 0.0.1

--- src/test/java/com/auth/wow/libre/CoreApplicationTests.java
+++ src/test/java/com/auth/wow/libre/CoreApplicationTests.java
@@ -1,12 +1,11 @@
 package com.auth.wow.libre;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
 
-    @Test
+
     void contextLoads() {
     }
 
