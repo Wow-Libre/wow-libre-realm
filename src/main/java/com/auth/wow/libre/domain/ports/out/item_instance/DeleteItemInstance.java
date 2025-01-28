2110bc51768108c29794b6246f3e852fd506ef52
Sebastian Chitiva <manuchitivar@gmail.com>
2025-01-28 14:02:49 -0500

    update

--- src/main/java/com/auth/wow/libre/domain/ports/out/item_instance/DeleteItemInstance.java
+++ src/main/java/com/auth/wow/libre/domain/ports/out/item_instance/DeleteItemInstance.java
@@ -3,5 +3,5 @@ package com.auth.wow.libre.domain.ports.out.item_instance;
 import com.auth.wow.libre.infrastructure.entities.characters.*;
 
 public interface DeleteItemInstance {
-    public void delete(ItemInstanceEntity instanceEntity, String transactionId);
+     void delete(ItemInstanceEntity instanceEntity, String transactionId);
 }
--- src/main/java/com/auth/wow/libre/infrastructure/repositories/characters/character_inventory/CharacterInventoryRepository.java
+++ src/main/java/com/auth/wow/libre/infrastructure/repositories/characters/character_inventory/CharacterInventoryRepository.java
@@ -11,5 +11,10 @@ import java.util.*;
 public interface CharacterInventoryRepository extends CrudRepository<CharacterInventoryEntity, Long> {
     Optional<CharacterInventoryEntity> findByGuidAndItem(Long guid, Long item);
 
-
+    @Query("SELECT new com.auth.wow.libre.domain.model.CharacterInventoryModel( " +
+            "chi.guid, chi.bag, chi.slot, chi.item, it.itemEntry, it.guid) " +
+            "FROM CharacterInventoryEntity chi " +
+            "INNER JOIN ItemInstanceEntity it ON chi.item = it.guid " +
+            "WHERE chi.guid = :guid")
+    List<CharacterInventoryModel> findByAllInventory(@Param("guid") Long guid);
 }
