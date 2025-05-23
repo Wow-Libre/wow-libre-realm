355221744d279c996cf3e178973308f9e11f9d1d
Sebastian Chitiva <manuchitivar@gmail.com>
2025-05-22 22:59:55 -0500

    Elimino al entidad de publicaciones

--- src/main/java/com/auth/wow/libre/domain/ports/in/server_publications/ServerPublicationsPort.java
+++
@@ -1,9 +0,0 @@
-package com.auth.wow.libre.domain.ports.in.server_publications;
-
-import com.auth.wow.libre.domain.model.dto.view.*;
-
-import java.util.*;
-
-public interface ServerPublicationsPort {
-    List<Card> publications();
-}
