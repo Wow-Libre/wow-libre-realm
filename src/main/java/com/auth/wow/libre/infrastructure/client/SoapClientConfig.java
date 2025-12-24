7459d60724eddc36e99d9d0d7e2885163e593cd8
Sebastian Chitiva <manuchitivar@gmail.com>
2025-12-23 21:20:21 -0500

    Comunicacion con el xoap

--- src/main/java/com/auth/wow/libre/infrastructure/client/SoapClientConfig.java
+++ src/main/java/com/auth/wow/libre/infrastructure/client/SoapClientConfig.java
@@ -47,7 +47,7 @@ public class SoapClientConfig {
         webServiceTemplate.setMessageSender(auth);
         webServiceTemplate.setInterceptors(new ClientInterceptor[]{new CustomLoggingInterceptor()});
 
-        webServiceTemplate.setDefaultUri("http://host.docker.internal:7878");
+        webServiceTemplate.setDefaultUri("http://172.17.0.1:7878");
         return webServiceTemplate;
     }
 
@@ -61,7 +61,7 @@ public class SoapClientConfig {
         webServiceTemplate.setMessageSender(auth);
         webServiceTemplate.setInterceptors(new ClientInterceptor[]{new CustomLoggingInterceptor()});
 
-        webServiceTemplate.setDefaultUri("http://host.docker.internal:7878");
+        webServiceTemplate.setDefaultUri("http://172.17.0.1:7878");
         return webServiceTemplate;
     }
 }
