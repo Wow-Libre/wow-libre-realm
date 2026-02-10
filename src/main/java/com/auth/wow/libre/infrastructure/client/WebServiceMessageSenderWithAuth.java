30e71d91a4bd467db7f849eeb09efa2743cf7678
Manuel sebastian chitiva <manuel.chitiva@mercadolibre.com.co>
2026-02-09 23:03:10 -0500

    log debuggear

--- src/main/java/com/auth/wow/libre/infrastructure/client/WebServiceMessageSenderWithAuth.java
+++ src/main/java/com/auth/wow/libre/infrastructure/client/WebServiceMessageSenderWithAuth.java
@@ -3,6 +3,8 @@ package com.auth.wow.libre.infrastructure.client;
 import com.auth.wow.libre.domain.model.GameMasterCredentials;
 import com.auth.wow.libre.infrastructure.conf.db.RealmSoapCredentialsProvider;
 import com.auth.wow.libre.infrastructure.context.RealmContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.ws.transport.http.HttpUrlConnectionMessageSender;
@@ -15,6 +17,8 @@ import java.util.Base64;
 @Configuration
 public class WebServiceMessageSenderWithAuth extends HttpUrlConnectionMessageSender {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebServiceMessageSenderWithAuth.class);
+
     private final RealmSoapCredentialsProvider soapCredentialsProvider;
 
     public WebServiceMessageSenderWithAuth(RealmSoapCredentialsProvider soapCredentialsProvider) {
@@ -26,8 +30,16 @@ public class WebServiceMessageSenderWithAuth extends HttpUrlConnectionMessageSen
         Long realmId = RealmContext.getCurrentRealmId();
         GameMasterCredentials gameMasterCredentials = soapCredentialsProvider.getCredentials(realmId);
 
+        String username = gameMasterCredentials.username();
+        String password = gameMasterCredentials.password();
+        LOGGER.info("SOAP auth realmId={} uri={} username={} passwordLength={}",
+                realmId,
+                connection.getURL(),
+                username,
+                password != null ? password.length() : 0);
+
         Base64.Encoder enc = Base64.getEncoder();
-        String credentials = String.format("%s:%s", gameMasterCredentials.username(), gameMasterCredentials.password());
+        String credentials = String.format("%s:%s", username, password);
         String encodedAuthorization = enc.encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
         connection.setRequestProperty(HttpHeaders.AUTHORIZATION, "Basic " + encodedAuthorization);
 
